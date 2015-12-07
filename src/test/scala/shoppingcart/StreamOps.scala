a163988d33270fac834233c122767da51d34061a
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-12-07 21:18:16 +0000

    Removed Shopping cart.

--- src/test/scala/shoppingcart/StreamOps.scala
+++
@@ -1,9 +0,0 @@
-package shoppingcart
-
-object StreamOps {
-  def unfold[A, B](seed: A)(f: A => Option[(B, A)]): Stream[B] =
-    f(seed) match {
-      case None => Stream.empty
-      case Some((b, a)) => Stream.cons(b, unfold(a)(f))
-    }
-}
