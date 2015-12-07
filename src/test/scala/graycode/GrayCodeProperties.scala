89124085ea2ff26fafb0b69b18136a11332d2575
Mark Harris <mark.harris@digital.cabinet-office.gov.uk>
2015-12-07 21:18:28 +0000

    A property template.

--- src/test/scala/graycode/GrayCodeProperties.scala
+++ src/test/scala/graycode/GrayCodeProperties.scala
@@ -1,8 +1,16 @@
 package graycode
 
-import org.scalacheck.Properties
+import org.scalacheck._
+import Prop.forAll
 import graycode.GrayCode._
 
 object GrayCodeProperties extends Properties("Binary reflected gray code") {
+  val numbersLessThan128 = Gen.posNum[Int].suchThat(_ < 128)
+
+  property("") = forAll(numbersLessThan128) {
+    forAll(numbersLessThan128) { (x: Int) =>
+      false
+    }
+  }
 
 }
--- src/test/scala/graycode/GrayCodeSpec.scala
+++ src/test/scala/graycode/GrayCodeSpec.scala
@@ -1,5 +1,6 @@
 package graycode
 
+import org.scalacheck.Gen
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.scalatest.{FlatSpec, Matchers}
 import graycode.GrayCode._
@@ -8,5 +9,12 @@ class GrayCodeSpec extends FlatSpec with GeneratorDrivenPropertyChecks with Matc
 
   behavior of "binary reflected Gray code"
 
+  val numbersLessThan128 = Gen.posNum[Int].suchThat(_ < 128)
+
+  it should "" in {
+    forAll(numbersLessThan128) { (x: Int) =>
+      fail
+    }
+  }
 
 }
